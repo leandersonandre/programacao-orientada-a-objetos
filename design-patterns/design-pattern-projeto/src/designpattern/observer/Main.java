db8a34c24cd3f11cb971d7589b0dddf7fe7cbc74
leanderson andre <leanderson.andre@gmail.com>
2019-11-08 20:18:36 -0300

    design patterns

---
+++ design-patterns/design-pattern-projeto/src/designpattern/observer/Main.java
@@ -0,0 +1,27 @@
+package designpattern.observer;
+
+public class Main {
+
+	 public static void main(String[] args) {
+		ConcreteSubject assunto = new ConcreteSubject();
+
+		ConcreteObserver o1 = new ConcreteObserver("Ferrari");
+		ConcreteObserver o2 = new ConcreteObserver("Lamborguini");
+		ConcreteObserver o3 = new ConcreteObserver("Mustang");
+		ConcreteObserver o4 = new ConcreteObserver("Fiat");
+
+		assunto.attach(o1);
+		assunto.attach(o2);
+		assunto.attach(o3);
+		assunto.attach(o4);
+		
+		assunto.generateNewNumber();
+		System.out.println("------");
+		assunto.detach(o3);
+		
+
+		assunto.generateNewNumber();
+		
+	}
+	
+}
---
+++ design-patterns/design-pattern-projeto/src/designpattern/observer/Observer.java
@@ -0,0 +1,7 @@
+package designpattern.observer;
+
+public interface Observer {
+	
+	void update(Subject s, Object o);
+
+}
